ebbd06a4bc91fb72d958a31a8482b2aa0a4a0fe0
IginlaIsrael <iginlaisraelo@gmail.com>
2024-09-21 22:00:44 +0100

    Adding_Calculator

---
+++ calculator.js
@@ -0,0 +1,38 @@
+function add(num1,num2){
+   return num1 + num2;
+}
+function min(num1,num2){
+    return num1 - num2;
+}
+function mul(num1,num2){
+    return num1*num2;
+}
+function div(num1,num2){
+    return num1 / num2;
+}
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1,number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+document.getElementById('subtract').addEventListener('click', function(){
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = min(number1,number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('multiply').addEventListener('click', function(){
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = mul(number1,number2);
+    document.getElementById('calculation-result').textContent = result;
+});
+
+document.getElementById('divide').addEventListener('click', function(){
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = div(number1,number2);
+    document.getElementById('calculation-result').textContent = result;
+});
