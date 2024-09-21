ca989b47cf2fd0b2739199139b81f0d8eff2233c
IginlaIsrael <iginlaisraelo@gmail.com>
2024-09-21 17:59:08 +0100

    first_quiz

---
+++ quiz.js
@@ -0,0 +1,13 @@
+function checkAnswer(){
+    const correctAnswer = "4";
+    const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+const result = document.querySelector('#feedback');
+    if(userAnswer === correctAnswer){
+        result.textContent ="Correct! Well done.";
+    } else {
+        result.textContent = "That's incorrect. Try again!";
+    }
+    
+}
+const btn = document.getElementById("submit-answer")
+btn.addEventListener("click",checkAnswer);
